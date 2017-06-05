f6e3439af1bddd40608460d5bdf6acfa60b8c480
jkvim <jkvim@outlook.com>
2017-06-05 17:11:35 +0800

    Step 6: Add iconfont config.

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 import './index.scss';
 import './index.html';
+import './icon.font.js';
--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,30 @@ module.exports = {
         test: /.html$/,
         loader: 'raw-loader',
       },
+      {
+        test: /icon\.font\.js$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'fontgen-loader',
+            options: {
+              embed: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|eot|ttf|svg)$/,
+        use: [
+          'url-loader',
+          'file-loader',
+        ],
+      },
     ],
   },
 };
