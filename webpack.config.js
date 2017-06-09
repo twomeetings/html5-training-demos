01fc9c27140aab0ba7e1fc009e50631dcb3584a0
jkvim <jkvim@outlook.com>
2017-06-09 18:31:41 +0800

    Simplify webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,12 @@
 const path = require('path');
-const webpack = require('webpack');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'index.js'),
   },
-  devtool: '#source-map',
-  output: {
-    path: '/dist',
-    filename: './bundle.js',
-  },
-  devServer: {
-    hot: true,
-    port: 8080,
-    compress: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
     new HTMLWebpackPlugin({
-      filename: './index.html',
       template: './index.html',
     }),
   ],
@@ -33,18 +15,6 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader?importLoaders=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /.html$/,
-        loader: 'raw-loader',
       },
     ],
   },
