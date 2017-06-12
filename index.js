35fe323dc778aeb1bad7c7b7088aad77faa5695d
xulianghui1 <twomeetings@gmail.com>
2017-06-12 14:53:55 +0800

    completed

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import './index.css'
 
-const abc = 1
-console.log('abc:',abc)
+const top=document.querySelector('#top')
+const innerTop=document.querySelector('#innerTop')
+const bottom = document.querySelector('#bottom')
+const innerBottom = document.querySelector('#innerBottom')
+
+top.addEventListener('click',function(){
+	console.log('top is clicked')
+})
+
+innerTop.addEventListener('click',function(){
+	console.log('innerTop is clicked')
+})
+
+bottom.addEventListener('click',function(){
+	console.log('bottom is clicked')
+}, true)
+
+innerBottom.addEventListener('click',function(){
+	console.log('innerBottom is clicked')
+}, true)
