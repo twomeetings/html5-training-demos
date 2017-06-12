90a4a81c33050c090182379b9cd69a8a2e4ef11f
xulianghui1 <twomeetings@gmail.com>
2017-06-12 13:33:03 +0800

    complte html5 demo4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 import './index.css'
+
+const startButton = document.querySelector('#btn_start')
+const tableTarget = document.querySelector('#tableInfo')
+
+startButton.addEventListener('click',()=>{
+	console.log('clicked')
+
+	const websockClient = new WebSocket("ws://localhost:7777");
+	websockClient.onmessage = (event)=>{
+		let result= JSON.parse(event.data);
+		let tr= document.createElement("tr");
+		Object.keys(result).forEach(e=>{
+			let tempTd = document.createElement("td");
+			tempTd.innerHTML = result[e]
+			tr.appendChild(tempTd)
+		})
+
+		tableTarget.appendChild(tr)
+	};
+})
