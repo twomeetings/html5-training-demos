c6896de9731ec901cbface6997cf060d4dda599d
xulianghui1 <twomeetings@gmail.com>
2017-06-12 15:43:52 +0800

    upgraded with autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader','sass-loader']
+        loaders: ['style-loader', 'css-loader','sass-loader', 'postcss-loader']
       },
     ],
   },
+  devServer:{
+    disableHostCheck: true
+  }
 };
