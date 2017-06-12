7df466b43ea269a8b58c56d6c7786044519c45e8
Chuan.Peng <cpeng@thoughtworks.com>
2017-06-12 16:56:59 +0800

    Finish storage.

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ import './index.scss';
     const wrapDom = document.querySelector('#imageWrap');
     const imageDom = document.querySelector('#imageViewer');
 
-    // 设置下载链接
-    const setDownloadLink = (link) => {
-        downloadDom.href = link;
-        downloadDom.style.display = 'block'
+    // 保存图片
+    const saveImage = (image) => {
+        localStorage.setItem('myImage', image);
     };
+    // 获取图片
+    const getImage = () => {
+        return localStorage.getItem('myImage');
+    };
+
+    // 获取初始化数据
+    const loadHandler = function () {
+        const image = getImage();
+        if (image) {
+            imageDom.src = image;
+        }
+    };
+
     // 拖拽事件回调
     const dropHandler = function (event) {
         event.preventDefault();
@@ -21,17 +33,25 @@ import './index.scss';
         const reader = new FileReader();
         reader.onload = function () {
             imageDom.src = this.result;
+            saveImage(this.result);
         };
         reader.readAsDataURL(file);
     };
+
     // 拖入事件回调
     const dragEnterHandler = function () {
         wrapDom.style.borderColor = 'rgb(36,216,19)';
     };
+
+    // 页面初始化
+    window.addEventListener('load', loadHandler);
+
     // 绑定拖拽事件
     wrapDom.addEventListener('drop', dropHandler);
+
     // 绑定拖入事件
     wrapDom.addEventListener('dragenter', dragEnterHandler);
+
     // 清除浏览器默认行为
     wrapDom.addEventListener('dragover', (e) => {
         e.preventDefault();
