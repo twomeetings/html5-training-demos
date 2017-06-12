d3788b3b6e0563b4c42f72ffa60feecb171a12cd
xulianghui1 <twomeetings@gmail.com>
2017-06-12 16:36:29 +0800

    init

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import './index.css'
 
-const abc = 1
-console.log('abc:',abc)
+const btn = document.querySelector('#btn')
+const popup = document.querySelector('#divWithTriangle')
+
+btn.addEventListener('click',()=>{
+	popup.style.display = popup.style.display ==='block'?'none':'block'
+})
