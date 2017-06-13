c3e4b53a92682c8e940b413caa6e463b4734e162
xulianghui1 <twomeetings@gmail.com>
2017-06-13 08:57:14 +0800

    completed

--- myCanvas.js
+++ myCanvas.js
@@ -10,3 +10,10 @@ ctx.fillStyle='green'
 ctx.fillRect(10,10,300,300)
 ctx.strokeStyle='blue'
 ctx.strokeRect(10, 10, 300, 300)
+
+let img = new Image()
+img.src='./html5_css3.jpg'
+
+img.onload = function(){
+	ctx.drawImage(img, 0,0, canvas.width,canvas.height)
+}
