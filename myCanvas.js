81150a4b01115ced011653061ffe9a716be323b1
xulianghui1 <twomeetings@gmail.com>
2017-06-13 08:43:04 +0800

    completed

--- myCanvas.js
+++ myCanvas.js
@@ -1,10 +1,12 @@
 console.log('my canvans')
 
 let canvas = document.querySelector('#canvas')
+canvas.width  = 800;
+canvas.height = 600;
+
 let ctx = canvas.getContext('2d')
 
 ctx.fillStyle='green'
-ctx.fillRect(10,10,100,100)
-
+ctx.fillRect(10,10,300,300)
 ctx.strokeStyle='blue'
-ctx.strokeRect(10, 10, 100, 100)
+ctx.strokeRect(10, 10, 300, 300)
